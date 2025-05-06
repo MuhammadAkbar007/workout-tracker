0fb9b65b0f7fd44bc93fdb88c27e73002707859f
MuhammadAkbar007 <fotismenos007@mail.ru>
2025-05-06 14:29:05 +0500

    prettied 🥨

--- src/main/java/uz/akbar/workoutTracker/service/implementation/AdminServiceImpl.java
+++ src/main/java/uz/akbar/workoutTracker/service/implementation/AdminServiceImpl.java
@@ -20,68 +20,70 @@ import java.util.UUID;
 @Service
 public class AdminServiceImpl implements AdminService {
 
-    @Autowired private UserRepository repository;
-    @Autowired private RoleRepository roleRepository;
+	@Autowired
+	private UserRepository repository;
+	@Autowired
+	private RoleRepository roleRepository;
 
-    @Override
-    @Transactional
-    public AppResponse blockUnblockUser(UUID userId, GeneralStatus status, UUID adminId) {
-        if (userId.equals(adminId)) throw new AppBadException("Wrong id: " + adminId);
+	@Override
+	@Transactional
+	public AppResponse blockUnblockUser(UUID userId, GeneralStatus status, UUID adminId) {
+		if (userId.equals(adminId))
+			throw new AppBadException("Wrong id: " + adminId);
 
-        User user =
-                repository
-                        .findById(userId)
-                        .orElseThrow(
-                                () -> new AppBadException("User not found with id = " + userId));
+		User user = repository
+				.findById(userId)
+				.orElseThrow(
+						() -> new AppBadException("User not found with id = " + userId));
 
-        user.setStatus(status);
-        repository.save(user);
+		user.setStatus(status);
+		repository.save(user);
 
-        return AppResponse.builder()
-                .success(true)
-                .message(
-                        "User has been "
-                                + (status == GeneralStatus.BLOCK ? "blocked" : "activated"))
-                .data(userId)
-                .build();
-    }
+		return AppResponse.builder()
+				.success(true)
+				.message(
+						"User has been "
+								+ (status == GeneralStatus.BLOCK ? "blocked" : "activated"))
+				.data(userId)
+				.build();
+	}
 
-    @Override
-    @Transactional
-    public AppResponse assignRole(UUID userId, RoleType roleType, UUID adminId) {
-        if (userId.equals(adminId)) throw new AppBadException("Wrong id: " + adminId);
+	@Override
+	@Transactional
+	public AppResponse assignRole(UUID userId, RoleType roleType, UUID adminId) {
+		if (userId.equals(adminId))
+			throw new AppBadException("Wrong id: " + adminId);
 
-        User user =
-                repository
-                        .findById(userId)
-                        .orElseThrow(
-                                () -> new AppBadException("User not found with id = " + userId));
+		User user = repository
+				.findById(userId)
+				.orElseThrow(
+						() -> new AppBadException("User not found with id = " + userId));
 
-        switch (roleType) {
-            case ROLE_ADMIN:
-                Role roleAdmin =
-                        roleRepository
-                                .findByRoleType(RoleType.ROLE_ADMIN)
-                                .orElseThrow(() -> new AppBadException("Admin role is not found"));
+		switch (roleType) {
+			case ROLE_ADMIN:
+				Role roleAdmin = roleRepository
+						.findByRoleType(RoleType.ROLE_ADMIN)
+						.orElseThrow(() -> new AppBadException("Admin role is not found"));
 
-                if (!user.getRoles().contains(roleAdmin)) user.getRoles().add(roleAdmin);
-                break;
-            case ROLE_USER:
-                user.getRoles().removeIf(role -> role.getRoleType().equals(RoleType.ROLE_ADMIN));
-                break;
+				if (!user.getRoles().contains(roleAdmin))
+					user.getRoles().add(roleAdmin);
+				break;
+			case ROLE_USER:
+				user.getRoles().removeIf(role -> role.getRoleType().equals(RoleType.ROLE_ADMIN));
+				break;
 
-            default:
-                throw new AppBadException("Wrong role type");
-        }
+			default:
+				throw new AppBadException("Wrong role type");
+		}
 
-        repository.save(user);
+		repository.save(user);
 
-        return AppResponse.builder()
-                .success(true)
-                .message(
-                        "User role has been changed to "
-                                + (roleType.equals(RoleType.ROLE_USER) ? " user" : " admin"))
-                .data(userId)
-                .build();
-    }
+		return AppResponse.builder()
+				.success(true)
+				.message(
+						"User role has been changed to "
+								+ (roleType.equals(RoleType.ROLE_USER) ? " user" : " admin"))
+				.data(userId)
+				.build();
+	}
 }
